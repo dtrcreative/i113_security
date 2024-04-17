263b8be956e8570372b87df7c3ebbb908c1a1472
drogozhnikov <DitarRogozhnikov@gmail.com>
2024-04-17 17:47:56 +0300

    InitialCommit

---
+++ src/main/java/com/micro/i113_security/I113SecurityApplication.java
@@ -0,0 +1,13 @@
+package com.micro.i113_security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class I113SecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(I113SecurityApplication.class, args);
+	}
+
+}
